0fa72f8a2cdd9167a6b107a0961a24c6780a5774
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-03-28 16:15:40 -0400

    login feature added

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,11 @@ exports.new = (req, res, next) =>
 
 exports.show = (req, res, next) =>
 {
-    res.render('./user/profile');
+    let id = req.session.user;
+    console.log(req.flash());
+    User.findById(id)
+    .then((user) => res.render('/users/profile', {user}))
+    .catch(err => next(err));
 }
 
 exports.logout = (req, res, next) =>
@@ -24,7 +28,39 @@ exports.logout = (req, res, next) =>
 
 exports.login = (req, res, next) =>
 {
-    res.render('./user/profile')
+     // authenticate user login request
+     let email = req.body.email;
+     let password = req.body.password;
+ 
+     // get the user that matches the email
+     User.findOne({email: email})
+     .then((user) =>
+     {
+         if (user)
+         {
+             // user found in db
+             user.compare(password)
+             if (user.compare(password))
+             {
+                req.session.user = user._id; // store user id in db
+                req.flash('success', 'You have successfully logged in.');
+                res.redirect('/users/profile')
+             }
+             else
+             {
+                req.flash('error', 'Wrong password.');
+                console.log('wrong password');
+                res.redirect('/users/login')
+             }
+        }
+         else
+         {
+            req.flash('error', 'Wrong email address.');
+            console.log("wrong email address")
+            res.redirect('/users/login')
+         }
+     })
+     .catch(err => next(err))
 }
 
 exports.create = (req, res, next) =>
@@ -32,7 +68,7 @@ exports.create = (req, res, next) =>
     let user = new User(req.body);
     console.log(user);
     user.save()
-    .then(() => res.redirect('/profile'))
+    .then(() => res.redirect('/users/profile'))
     .catch(err => 
         {
             if (err.name === 'ValidationError')
