9a0dcebc9001991cf72e322c260f512c003bdbf7
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-03-28 15:01:43 -0400

    form added, css updated and controller refactored

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,28 @@
 const model = require('../models/user');
 
-exports.index = (req, res, next)=>{
-    //res.send('send all stories');
-    model.find()
-    .then(stories=>res.render('./user/index', {stories}))
-    .catch(err=>next(err));
+exports.index = (req, res, next) => 
+{
+    res.render('./user/login');
+}
+
+exports.new = (req, res, next) => 
+{
+    //res.send('go to login');
+
+    res.render('./user/new');
 };
+
+exports.show = (req, res, next) =>
+{
+    res.render('./user/profile');
+}
+
+exports.logout = (req, res, next) =>
+{
+    res.redirect('/login');
+}
+
+exports.login = (req, res, next) =>
+{
+    res.redirect('/login');
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const controller = require('../controllers/userController');
 
-//GET /stories: send all stories to the user
 const router = express.Router();
 
-//GET /stories: send all stories to the user
+//GET /users/new: go to signup page
 
-router.get('/', controller.index);
+router.get('/new', controller.new);
+
+// GET /users/login go to login page
+
+router.get('/login', controller.index);
+
+// GET /users/profile go to login page
+
+router.get('/profile', controller.show);
+
+// GET /users/logout logout user
+
+router.get('/logout', controller.logout);
+
+router.post('/', controller.login);
 
 module.exports = router;
